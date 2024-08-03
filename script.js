21a940cfa7294a9741b53821b0f22450170a34eb
Vladimir Kutepov <vl.kutepov@gmail.com>
2024-08-03 17:37:03 +1000

    finish project

--- script.js
+++ script.js
@@ -10,4 +10,75 @@ function Book(title, author, pages, read) {
 function addBook(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   library.push(newBook);
+  displayBooks();
 }
+
+function displayBooks() {
+  const booksDisplay = document.querySelector('main');
+  booksDisplay.innerHTML = '';
+
+  library.forEach((book, index) => {
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('book');
+
+    const readStatus = book.read ? 'read' : 'unread';
+    const readText = book.read ? 'Прочитано' : 'В процессе'
+
+    bookElement.innerHTML = `
+      <div class="title">${book.title}</div>
+      <div class="author">${book.author}</div>
+      <div class="pages">${book.pages}</div>
+      <div class="${readStatus} status-toggle no-select" data-index="${index}">${readText}</div>
+      <button class="delete-book" data-index="${index}">&times;</button>
+    `;
+
+    booksDisplay.appendChild(bookElement);
+  });
+
+  const statusElements = document.querySelectorAll('.status-toggle');
+  statusElements.forEach(element => {
+    element.addEventListener('click', () => {
+      const index = element.getAttribute('data-index');
+      library[index].read = !library[index].read;
+      displayBooks();
+    });
+  });
+
+  const deleteButton = document.querySelectorAll('.delete-book');
+  deleteButton.forEach(button => {
+    button.addEventListener('click', (event) => {
+      const index = button.getAttribute('data-index');
+      library.splice(index, 1);
+      displayBooks();
+      event.stopPropagation();      
+    });
+  });
+}
+
+
+
+document.querySelector('.add-book').addEventListener('click', () => {
+  document.querySelector('.book-form').classList.toggle('active')
+  ;
+});
+
+document.querySelector('.close').addEventListener('click', () => {
+  document.querySelector('.book-form').classList.remove('active');
+});
+
+document.querySelector('.form-book').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const title = document.querySelector('#title').value;
+  const author = document.querySelector('#author').value;
+  const pages = document.querySelector('#pages').value;
+  const read = document.querySelector('#read').checked;
+
+  addBook(title, author, pages, read);
+
+  document.querySelector('.book-form').classList.remove('active');
+  document.querySelector('.form-book').reset();
+});
+
+
+displayBooks();
